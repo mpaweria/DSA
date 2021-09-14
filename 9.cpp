8d5aadcd1907b86fe4693ba1ff1c9e6fc5a5dc1f
mpaweria <ms.paweria@gmail.com>
2021-09-14 22:52:06 +0530

    ques 9

---
+++ 9.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+    vector<int> luckyNumber;
+    // if there is only one element then return that element
+    if(matrix.size()==1 && matrix[0].size()==1)
+    {
+        luckyNumber.push_back(matrix[0][0]);
+        return luckyNumber;
+    }
+
+    // i denotes the row and j denotes the column
+    for(int i=0; i<matrix.size(); i++)
+    {
+        int col=0, rowMin=matrix[i][0];
+        for(int j=0; j<matrix[i].size(); j++)
+        {
+            if(rowMin > matrix[i][j])
+            {
+                col=j;
+                rowMin = matrix[i][j];
+            }
+        }
+        // j denotes the row index and col denotes the column index
+        int colMax=matrix[0][col];
+        for(int j=0; j<matrix.size(); j++)
+        {
+            if(colMax < matrix[j][col])
+                colMax = matrix[j][col];
+            
+        }
+        if(rowMin==colMax)
+            luckyNumber.push_back(rowMin);
+    }
+    return luckyNumber;
+}
+void print(vector<int> vec)
+{
+    for(auto v: vec)
+        cout << v;
+}
+int main()
+{
+    vector<vector<int>> vec = {{1,10,4,2},{9,3,8,7},{15,16,17,12}};
+    print(luckyNumbers(vec));
+
+    return 0;
+}
