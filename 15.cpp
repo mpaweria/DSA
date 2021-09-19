d73d4b5727cfae33c8309b9ad63f5a62fbb71731
mpaweria <ms.paweria@gmail.com>
2021-09-19 23:36:27 +0530

    ques 15

---
+++ 15.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int countNegatives(vector<vector<int>>& grid) {
+    int count = 0;
+    for(int i=0; i<grid.size(); i++)
+    {
+        for(int j=0; j<grid[i].size(); j++)
+        {
+            if(grid[i][j]<0)
+                count++;
+        }
+    }        
+    return count;
+}
+
+int main()
+{
+    vector<vector<int>> vec = {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+    cout << countNegatives(vec);
+
+    return 0;
+}
