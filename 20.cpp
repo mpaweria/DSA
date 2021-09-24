117a0ed4f90f4cf3ef6028657f043bcaf8d938de
mpaweria <ms.paweria@gmail.com>
2021-09-24 22:22:36 +0530

    ques 21

---
+++ 20.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/build-array-from-permutation/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> buildArray(vector<int>& nums) {
+    vector<int> ans(nums.size(), 0);
+    for(int i=0; i<nums.size(); i++)
+    {
+        ans[i]=nums[nums[i]];
+    }        
+    return ans;
+}
+int main()
+{
+    vector<int> vec = {5,0,1,2,3,4}, ans = buildArray(vec);
+
+    for(auto i: ans)
+    { 
+        cout << i << " ";
+    }
+    return 0;
+}
---
+++ 21.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int finalValueAfterOperations(vector<string>& operations) {
+    int ans = 0;
+    for(int i=0; i<operations.size(); i++)
+    {
+        if(operations[i]=="X++" || operations[i]=="++X")
+            ans++;
+        else if(operations[i]=="X--" || operations[i]=="--X")
+            ans--;
+    }
+    return ans;
+}
+int main()
+{
+    vector<string> vec = {"++X","++X","X++"};
+
+    cout << finalValueAfterOperations(vec);
+}
