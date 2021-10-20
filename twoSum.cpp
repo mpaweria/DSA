d8db020723703d473bf200308073ed3b2ce2ad56
mpaweria <ms.paweria@gmail.com>
2021-10-20 20:35:13 +0530

    Two Sum

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,29 +1,29 @@
-// //   https://leetcode.com/problems/two-sum/
+//  https://leetcode.com/problems/two-sum/
 
 #include<iostream>
 #include<vector>
 using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> vec;
+    vector<int> ans;
     for(int i=0; i<nums.size()-1; i++)
     {
         for(int j=i+1; j<nums.size(); j++)
         {
             if(nums[i]+nums[j]==target)
             {
-                vec.push_back(i);
-                vec.push_back(j);
+                ans.push_back(i);
+                ans.push_back(j);
                 break;
             }
         }
-    }
-    return vec;
+    }        
+    return ans;
 }
 int main()
 {
-    int num = 6;
-    vector<int> vec = {3,4}, ans=twoSum(vec, num);
+    int num = 9;
+    vector<int> vec = {2,7,11,15}, ans=twoSum(vec, num);
 
     for(auto v: ans)
     {
