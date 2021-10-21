86b2f205107005c7bd035f6323fc2ce24435ac8b
mpaweria <ms.paweria@gmail.com>
2021-10-21 23:39:01 +0530

    transpose

---
+++ transposeMatrix.cpp
@@ -0,0 +1,33 @@
+//  https://leetcode.com/problems/transpose-matrix/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+    vector<vector<int>> trans(matrix[0].size(), vector<int>(matrix.size(), 0));
+    for(int i=0; i<matrix.size(); i++)
+    {
+        for(int j=0; j<matrix[i].size(); j++)
+        {
+            trans[j][i] = matrix[i][j];
+        }
+    }
+    return trans;
+}
+int main()
+{
+    vector<vector<int>> vec = {{1,2,3},{4,5,6},{7,8,9}};
+    vector<vector<int>> ans = transpose(vec);
+
+    for(auto i: ans)
+    {
+        for(auto j: i)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
