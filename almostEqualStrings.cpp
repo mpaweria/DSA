a55731b6463c6cdd4dec0da557563d0a318ce5db
mpaweria <ms.paweria@gmail.com>
2022-01-10 23:48:12 +0530

    streak

---
+++ almostEqualStrings.cpp
@@ -0,0 +1,8 @@
+//  https://leetcode.com/problems/check-whether-two-strings-are-almost-equivalent/
+
+#include<iostream>
+using namespace std;
+
+bool checkAlmostEquivalent(string word1, string word2) {
+            
+}
