b54ae27afea2282f8bfa8e54bac838bb8f2ebf4d
mpaweria <ms.paweria@gmail.com>
2022-01-11 20:19:21 +0530

    Almost Equivalent Strings

--- almostEqualStrings.cpp
+++ almostEqualStrings.cpp
@@ -1,8 +1,31 @@
 //  https://leetcode.com/problems/check-whether-two-strings-are-almost-equivalent/
 
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 bool checkAlmostEquivalent(string word1, string word2) {
-            
+    int charCount[26] = {0};
+
+    for(int i=0; i<word1.size(); i++)
+    {
+        charCount[word1[i] - 'a']++;
+        charCount[word2[i] - 'a']--;
+    }
+
+    for(int i=0; i<26; i++)
+    {
+        if(charCount[i] < -3 || charCount[i] > 3)
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    string s1 = "abcdeef";
+    string s2 = "abaaacc";
+
+    cout << checkAlmostEquivalent(s1, s2);
+
+    return 0;
 }
